ff0935b7709f444b05423be33cfbe2401a2fb68b
Alex <57383610+Takoyaki92@users.noreply.github.com>
2021-08-24 15:47:55 +0900

    Update transport_test.go

--- transport/transport_test.go
+++ transport/transport_test.go
@@ -1,6 +1,7 @@
 package transport_test
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -23,9 +24,9 @@ func TestGetGames(t *testing.T) {
 		t.Errorf("Handler returned wrong status code: got %v, wanted %v", status, http.StatusOK)
 	}
 
-	expected := `[{"id":"1","title":"Sonic Unleashed","developer":"Sonic Team","rating":"5"},{"id":"2","title":"Sonic 2","developer":"Sega","rating":"10"},{"id":"3","title":"Sonic 3","developer":"Sega","rating":"10"}]`
+	expected := "\"Success\"\n"
 	if rr.Body.String() != expected {
-		t.Errorf("Handler returned unexpected body: got %v, wanted %v", rr.Body.String(), expected)
+		t.Errorf("Handler returned unexpected body: got [%v], wanted [%v]", rr.Body.String(), expected)
 	}
 }
 
@@ -46,9 +47,85 @@ func TestGetGame(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `{"id":"1","title":"Sonic Unleashed","developer":"Sonic Team","rating":"5"}`
+	expected := "\"Game\"\n"
 	if rr.Body.String() != expected {
 		t.Errorf("Handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
+}
+
+func TestCreateGame(t *testing.T) {
+	// create new HTTP post request
+	var json = []byte(`{"title": "title", "developer": "developer", "rating": "rating"}`)
+	req, _ := http.NewRequest("POST", "/api/games", bytes.NewBuffer(json))
+	// assign HTTP handler
+	handler := http.HandlerFunc(transport.CreateGame)
+	// record response
+	rr := httptest.NewRecorder()
+	// dispatch http request
+	handler.ServeHTTP(rr, req)
+	// add assertions
+	status := rr.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"id":"","title":"title","developer":"developer","rating":"rating"}
+`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+}
+
+func TestDeleteGame(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/api/games", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("id", "1")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(transport.DeleteGame)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := ""
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestUpdateGame(t *testing.T) {
+	// create new HTTP post request
+	var json = []byte(`{"title": "title", "developer": "developer", "rating": "rating"}`)
+	req, _ := http.NewRequest("PUT", "/api/games", bytes.NewBuffer(json))
+	// assign HTTP handler
+	handler := http.HandlerFunc(transport.UpdateGame)
+	// record response
+	rr := httptest.NewRecorder()
+	// dispatch http request
+	handler.ServeHTTP(rr, req)
+	// add assertions
+	status := rr.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"id":"","title":"title","developer":"developer","rating":"rating"}
+`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
 }
